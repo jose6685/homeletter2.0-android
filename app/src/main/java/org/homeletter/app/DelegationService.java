51b7bfc748a679f0432fe8f9afbb347c4abe3c07
jose6685 <jose6685@gmail.com>
2025-10-03 13:43:57 +0800

    初始提交：整合 AdMob Rewarded、Manifest 與依賴

---
+++ app/src/main/java/org/homeletter/app/DelegationService.java
@@ -0,0 +1,14 @@
+package org.homeletter.app;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
